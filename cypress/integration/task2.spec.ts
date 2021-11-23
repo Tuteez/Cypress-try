c448981c9e9ea041d7b652cdba20e2d2ba05a19c
Maksim Trojan <34544978+Tuteez@users.noreply.github.com>
2021-11-23 23:47:05 +0200

    add gitignore

--- cypress/integration/task2.spec.ts
+++ cypress/integration/task2.spec.ts
@@ -5,15 +5,16 @@ import { api, cookies } from "../helpers/Helpers";
 import { ProductCard } from "../page-objects/ProductCard";
 import { ProductPage } from "../page-objects/ProductPage";
 
-describe('Task 2. TC1. Product Card', () => {
+describe('Task 2. Product Card', () => {
   let productCard = new ProductCard;
   let product = new ProductPage
+  
   beforeEach(() => {
     cookies.setRegion()
     api.loginAndOpenHomepage('auto@test.com', 'Password1')
   })
 
-  it('product card', () => {
+  it('should show all details in productcard and in product', () => {
     // TODO: find a better way, at least this one better than find by text, would be good to add here and elsewhere data-qa attributes.
     // Looks weird that Special offers and Recommended promotions has similar href
     cy.xpath(productCard.specialOffersLocatorXpath).should('be.visible')
